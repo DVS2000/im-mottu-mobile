76529e02f74cc0de70c72783e5051eba9d947aff
dvs2000 <dorivaldodossantos2000@gmail.com>
2024-07-09 23:19:04 +0100

    Get Characters Repository Imp

---
+++ lib/data/repository/get_characters_repository_imp.dart
@@ -0,0 +1,16 @@
+import 'package:dartz/dartz.dart';
+import 'package:marvel/data/datasource/remote/get_characters_remote_datasource/get_characters_remote_datasource.dart';
+import 'package:marvel/domain/entities/character_entity.dart';
+import 'package:marvel/domain/exceptions/character_exception.dart';
+import 'package:marvel/domain/repositories/get_characters_repository.dart';
+
+class GetCharactersRepositoryImp implements GetCharactersRepository {
+  final GetCharactersRemoteDatasource _datasource;
+  GetCharactersRepositoryImp(this._datasource);
+
+  @override
+  Future<Either<CharacterException, List<CharacterEntity>>> call({required int offset, required int limit}) {
+    return _datasource(offset: offset, limit: limit);
+  }
+  
+}
