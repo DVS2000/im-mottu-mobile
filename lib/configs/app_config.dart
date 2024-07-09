ebf32e2cf8d8feade805744ed15054cfda85d827
dvs2000 <dorivaldodossantos2000@gmail.com>
2024-07-09 21:51:52 +0100

    Configs API

---
+++ lib/configs/app_config.dart
@@ -0,0 +1,11 @@
+class AppConfigs {
+
+  /* INSTÂNCIA PRIVADA PARA USAR O PADRÃO SINGLETON */
+  AppConfigs._();
+
+  static const baseUrl = "https://gateway.marvel.com:443";
+  static const apiURl  = "$baseUrl/v1/public";
+
+  static const marvelPublicKey  = "d7ccc33629b216a02af01f9a48ad85b9";
+  static const marvelPrivateKey = "11504170115215020d9a796acb2b325f9d03d023";
+}
