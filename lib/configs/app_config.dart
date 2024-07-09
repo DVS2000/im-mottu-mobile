4f852371cc86c9003e21ce8a0d437458808401a9
dvs2000 <dorivaldodossantos2000@gmail.com>
2024-07-09 22:57:05 +0100

    Get characters remote datasource

--- lib/configs/app_config.dart
+++ lib/configs/app_config.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+
 class AppConfigs {
 
   /* INSTÂNCIA PRIVADA PARA USAR O PADRÃO SINGLETON */
@@ -8,4 +12,13 @@ class AppConfigs {
 
   static const marvelPublicKey  = "d7ccc33629b216a02af01f9a48ad85b9";
   static const marvelPrivateKey = "11504170115215020d9a796acb2b325f9d03d023";
+
+  static String getHashMd5() {
+    return md5.convert(
+      utf8.encode(
+        "${DateTime.now()}$marvelPrivateKey$marvelPublicKey"
+      )
+    ).toString();
+  }
+
 }
---
+++ lib/data/datasource/remote/get_characters_remote_datasource/get_characters_remote_datasource_imp.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:dartz/dartz.dart';
+import 'package:marvel/configs/app_config.dart';
+import 'package:marvel/data/dtos/character_dto.dart';
+import 'package:marvel/domain/entities/character_entity.dart';
+import 'package:marvel/domain/exceptions/character_exception.dart';
+import 'package:marvel/drivers/client_http_driver/client_http_driver.dart';
+import 'get_characters_remote_datasource.dart';
+
+class GetCharactersRemoteDatasourceImp implements GetCharactersRemoteDatasource {
+  final ClientHttpDriver _clientHttpDriver;
+  GetCharactersRemoteDatasourceImp(this._clientHttpDriver);
+
+  @override
+  Future<Either<CharacterException, List<CharacterEntity>>> call({required int offset, required int limit}) async {
+    final response = await _clientHttpDriver.get(
+      route: "/characters?ts=${DateTime.now()}&apikey=${AppConfigs.marvelPublicKey}&hash=${AppConfigs.getHashMd5()}&offset=$offset&limit=$limit", 
+      headers: { "Content-Type": "application/json" }
+    );
+
+    if(response.statusCode == 200) {
+      return Right(
+        List<CharacterEntity>.from(
+          json.decode(response.body)["data"]["results"].map(
+            (x) => CharacterDto.fromJSon(x)
+          )
+        )
+      );
+
+    } else {
+      return Left(CharacterException(response.statusCode, "Ocorreu um erro, tente mais tarde"));
+      
+    }
+    
+  }
+  
+}
