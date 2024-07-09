68faa95799e6a081f60e893566cbc4b5a4b21b1e
dvs2000 <dorivaldodossantos2000@gmail.com>
2024-07-09 23:24:57 +0100

    Search Characters By Name Repository Imp

---
+++ lib/data/repository/search_characters_by_name_repository_imp.dart
@@ -0,0 +1,15 @@
+import 'package:dartz/dartz.dart';
+import 'package:marvel/data/datasource/remote/search_characters_by_name_remote_datasource/search_characters_by_name_remote_datasource.dart';
+import 'package:marvel/domain/entities/character_entity.dart';
+import 'package:marvel/domain/exceptions/character_exception.dart';
+import 'package:marvel/domain/repositories/search_characters_by_name_repository.dart';
+
+class SearchCharactersByNameRepositoryImp implements SearchCharactersByNameRepository {
+  final SearchCharactersByNameRemoteDatasource _datasource;
+  SearchCharactersByNameRepositoryImp(this._datasource);
+
+  @override
+  Future<Either<CharacterException, List<CharacterEntity>>> call({required String name, required int offset, required int limit}) {
+    return _datasource(name: name, offset: offset, limit: limit);
+  }
+}
